f814f394dd20b6d923053cc66156e895481193ca
Akanksha Patil <akankshapatil1206@gmail.com>
2025-03-12 15:20:24 +0530

    Add files via upload

---
+++ divisible.java
@@ -0,0 +1,14 @@
+
+public class divisible {
+	
+	public static void main(String[] args) {
+		int num=45;
+		
+		if(num%5==0 && num%10==0) {
+			System.out.println("It is divisible by 5 and 10");
+		}else {
+			System.out.println("not divisble");
+		}
+	}
+
+}
